2e5d23cefd30804c6fdce2d0c1d32c38c11a804b
Johnnatan <romerozapatajonathan@gmail.com>
2021-12-30 17:56:03 -0500

    Repository

--- ParkRestfulApi/ParkApi/Data/ApplicationDbContext.cs
+++ ParkRestfulApi/ParkApi/Data/ApplicationDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ParkApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ParkApi.Data
 {
--- ParkRestfulApi/ParkApi/Models/Dtos/NationalParkDto.cs
+++ ParkRestfulApi/ParkApi/Models/Dtos/NationalParkDto.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
-namespace ParkApi.Models
+namespace ParkApi.Models.Dtos
 {
-    public class NationalPark
+    public class NationalParkDto
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
-        [Required]
         public string State { get; set; }
 
         public DateTime EstablishedOn { get; set; }
